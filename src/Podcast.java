987d8498e89291b29cb3335039eda3c6743bd2d8
MichaelZhou334 <99850678+MichaelZhou334@users.noreply.github.com>
2022-06-16 22:26:04 -0400

    Updated Podcast.java with documentation

--- src/Podcast.java
+++ src/Podcast.java
@@ -4,6 +4,9 @@
  * back-end of music streaming app: podcast
  * group project with James Wong and Jessica Lu
  *
+ * The class is where all the episodes are stored, while also
+ * having itself attributes that provides use to the users
+ *
  * Course: ICS4U1
  * Date: May 2, 2022
  * @author Michael Zhou
@@ -16,28 +19,45 @@ public class Podcast {
     /*
     Attributes
     */
+
+    /* the name of the podcast */
     private String name;
 
+    /* whether the podcast is followed */
     private boolean isFollowed;
 
+    /* the rating of the podcast */
     private double rating;      //cannot be controlled in the main
 
+    /* the amount of times the podcast is rated */
     private int amountRated;      //cannot be controlled in the main
 
+    /* whether the notification is turned on  */
     private boolean isNotification;
 
+    /* the episodes in the podcast */
     private ArrayList<String> episodeList = new ArrayList<>();
 
     /*
     Constructor
     */
 
+    /**
+     * Name: Podcast
+     * Description: Creates a new podcast, with aspects for the user such as the name, whether its followed and whether the notification
+     * is turned on or not
+     * @param name name of the podcast
+     * @param isFollowed whether the podcast is followed
+     * @param isNotification whether notifications is turned on
+     */
     public Podcast(String name, boolean isFollowed, boolean isNotification) {
 
+        //variables
         this.name = name;
         this.isFollowed = isFollowed;
         this.isNotification = isNotification;
 
+        //variables that cannot be changed manually
         this.amountRated = 0;
         this.rating = 0;
     }
@@ -46,18 +66,34 @@ public class Podcast {
     Accessors
     */
 
+    /**
+     * Gets the name of the podcast
+     * @return the name of the podcast
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Gets whether the podcast is followed
+     * @return the follower status of the podcast
+     */
     public boolean getIsFollowed()   {
         return this.isFollowed;
     }
 
+    /**
+     * Gets whether the podcast's notification is turned on
+     * @return the podcast's notification status
+     */
     public boolean getIsNotification()  {
         return this.isNotification;
     }
 
+    /**
+     * Gets the rating of the podcast, calculated by the average rating from the ratePodcast method
+     * @return the rating of the podcast
+     */
     public double getRating()   {
         if (this.rating == 0 )  {
             System.out.println("Not yet been rated");
@@ -67,10 +103,18 @@ public class Podcast {
         }
     }
 
+    /**
+     * Gets the amount of times the podcast is rated through the ratePodcast method
+     * @return the amount of times the podcast is rated
+     */
     public int getAmountRated() {
         return this.amountRated;
     }
 
+    /**
+     * Gets the list of Episodes
+     * @return the list of episodes
+     */
     public ArrayList<String> getEpisodeList()  {
         return this.episodeList;
     }
@@ -79,14 +123,26 @@ public class Podcast {
     Mutators
     */
 
+    /**
+     * Changes the name of the podcast
+     * @param newName the new name of the podcast
+     */
     public void setName(String newName) {
         this.name = newName;
     }
 
+    /**
+     * Changes the followed status of the podcast
+     * @param newIsFollowed the new followed status of the podcast
+     */
     public void setIsFollowed(boolean newIsFollowed)    {
         this.isFollowed = newIsFollowed;
     }
 
+    /**
+     * Changes the notification status of the podcast
+     * @param newIsNotification the new notification status of the podcast
+     */
     public void setIsNotification(boolean newIsNotification)    {
         this.isNotification = newIsNotification;
     }
@@ -95,6 +151,10 @@ public class Podcast {
     Other Methods
     */
 
+    /**
+     * Rates the podcast with a rating of 1-5 stars
+     * @param rateWithStars the rating you would give to the podcast
+     */
     public void ratePodcast(int rateWithStars)  {
         if (rateWithStars > 0 && rateWithStars < 6) {
             amountRated++;
@@ -105,14 +165,25 @@ public class Podcast {
 
     }
 
+    /**
+     * Add an episode to the podcast
+     * @param episode the episode that is added to the podcast
+     */
     public void addEpisode(Episode episode)    {
         this.episodeList.add(episode.getName());
     }
 
+    /**
+     * sort the episodes by alphabetical order by their name
+     */
     public void sortEpisodesAlphabetically()  {
         Collections.sort(episodeList);
     }
 
+    /**
+     * displays the attributes of the podcast
+     * @return the attributes of the podcast
+     */
     public String toString()    {
         String attributes;
         String ratingErrorDetection;
@@ -130,14 +201,15 @@ public class Podcast {
     //testing every method
     public static void main(String[] args) {
 
-        //constructors
+        //constructors for new episode classes
         Episode episode1 = new Episode ("Michael", true, false );
         Episode episode2 = new Episode ("Jessica", false, true);
         Episode episode3 = new Episode ("James", true, false);
 
+        //constructors for podcast classes
         Podcast podcast = new Podcast("ICS Podcast", true, true);
 
-        //accessors
+        //testing accessors
         System.out.println("Accessors");
         System.out.println("Name: " + podcast.getName());
         System.out.println("Is it Followed: " + podcast.getIsFollowed());
@@ -147,38 +219,34 @@ public class Podcast {
         System.out.println("Not filled in Ep list: " + podcast.getEpisodeList());
         System.out.println();
 
-        //mutators
+        //testing mutators
         System.out.println("Mutators");
-
         podcast.setName("Bad ICS Podcast");
         System.out.println(podcast.getName());
-
         podcast.setIsFollowed(false);
         System.out.println(podcast.getIsFollowed());
-
         podcast.setIsNotification(false);
         System.out.println(podcast.getIsNotification());
         System.out.println();
 
-        //other methods
+        //testing the other methods
         System.out.println("Other Methods");
-
         System.out.println("Demo: Rating the Podcast");
-
-        podcast.ratePodcast(5);     //works
+        podcast.ratePodcast(5);     //works as it is within 1-5 stars
         System.out.println(podcast.getRating());
         podcast.ratePodcast(2);     //rating averages out
         System.out.println(podcast.getRating());
-        podcast.ratePodcast(234);       //does not work
+        podcast.ratePodcast(234);       //does not work as it is not within 1-5 stars
         System.out.println(podcast.getRating());
         System.out.println();
 
-        //Demo: Add Episodes to Podcast
+        //Add Episodes to Podcast
         podcast.addEpisode(episode1);
         podcast.addEpisode(episode2);
         podcast.addEpisode(episode3);
         System.out.println();
 
+        //Sorting the episodes alphabetically
         System.out.println("Demo: Sorting Alphabetically");
         System.out.println("Without Sorting");
         System.out.println(podcast.getEpisodeList());
@@ -187,6 +255,7 @@ public class Podcast {
         System.out.println(podcast.getEpisodeList());
         System.out.println();
 
+        //Demonstrating toString
         System.out.println("Demonstrating toString()");
         System.out.println(podcast.toString());
 
